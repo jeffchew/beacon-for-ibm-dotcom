389ce2470533371e4dce90f70887db626c5c7d3e
Jeff Chew <jeff.chew@gmail.com>
2021-11-18 09:46:15 -0500

    fix(chrome): switch flag to `--disable-gpu`

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ module.exports = (args) => {
 async function runLightHouse(args) {
   try {
     const chrome = await chromeLauncher.launch({
-      chromeFlags: ['--headless', '--disable-dev-shm-usage', '--no-sandbox'],
+      chromeFlags: ['--headless', '--disable-gpu', '--no-sandbox'],
     });
     const options = {
       logLevel: 'info',
